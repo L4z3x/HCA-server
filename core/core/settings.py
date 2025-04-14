f3a40035575d6b0440a144dc0dd5a834e903b18f
L4z3x <moussaousselmal1970@gmail.com>
2025-04-14 21:00:09 +0100

    added cor middleware

--- core/core/settings.py
+++ core/core/settings.py
@@ -161,6 +161,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
 ]
 
