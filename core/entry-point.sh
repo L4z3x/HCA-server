962f108ef3bec36ab47d326601aac82e47c88c62
L4z3x <moussaousselmal1970@gmail.com>
2025-04-17 15:13:50 +0100

    fixed entry point and added migration

--- core/entry-point.sh
+++ core/entry-point.sh
@@ -11,28 +11,30 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 cd "$SCRIPT_DIR" || exit
 pwd
 
-echo  "${CYAN}==> Checking .env${NC}"
+echo -e  "${CYAN}==> Checking .env ${NC}"
 if [ ! -d "../../.env" ]; then
-  echo  "${RED}==> .env file not found!${NC}"
-  echo  "${YELLOW}==> Creating .env file...${NC}"
+  echo -e  "${RED}==> .env file not found!${NC}"
+  echo -e "${YELLOW}==> Creating .env file...${NC}"
   python3 -m venv ../../.env
   source .env/bin/activate
 fi
 source .env/bin/activate
 
-echo  "${CYAN}==> Updating dependencies${NC}"
-
+echo -e "${CYAN}==> Updating dependencies${NC}"
 pip install -r ../requirements.txt 
 
 
 # create tables
-echo  "${CYAN}==> Updating Database Tables${NC}"
+echo -e "${CYAN}==> Updating Database Tables${NC}"
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py spectacular --file schema.yml
-echo  "${GREEN}==> The Database has been updated${NC}"
+echo -e  "${GREEN}==> The Database has been updated${NC}"
 
-echo  "${YELLOW}==> Creating superuser...${NC}"
+echo -e "${YELLOW}==> Creating superuser...${NC}"
 # if [ "$CONTAINER_NAME" = "django-app" ]; then
 cat create_superuser.py | python3 manage.py shell
 # fi
+echo -e "${GREEN}==> Starting the Server${NC}"
+python3 manage.py runserver
+
